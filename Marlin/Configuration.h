d5b8b4905d5b07c95e34d94c1a8e5755ee1a2c76
Emanuel Garcês <effgarces@gmail.com>
2020-08-08 10:30:51 +0100

    Re-added ABS preheat

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1667,6 +1667,11 @@
 #define PREHEAT_2_TEMP_BED    70
 #define PREHEAT_2_FAN_SPEED   255 // Value from 0 to 255
 
+#define PREHEAT_3_LABEL       "ABS"
+#define PREHEAT_3_TEMP_HOTEND 230
+#define PREHEAT_3_TEMP_BED    110
+#define PREHEAT_3_FAN_SPEED   0 // Value from 0 to 255
+
 /**
  * Nozzle Park
  *
