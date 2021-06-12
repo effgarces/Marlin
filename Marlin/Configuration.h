0700a3d9a387649ef6de41c4e307d80f324d4035
Emanuel Garcês <effgarces@gmail.com>
2021-06-12 23:39:16 +0100

    Increase bed probing speed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1070,10 +1070,10 @@
 #define XY_PROBE_FEEDRATE (133*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
-#define Z_PROBE_FEEDRATE_FAST (2*60)
+#define Z_PROBE_FEEDRATE_FAST (4*60)
 
 // Feedrate (mm/min) for the "accurate" probe of each point
-#define Z_PROBE_FEEDRATE_SLOW (Z_PROBE_FEEDRATE_FAST / 4)
+#define Z_PROBE_FEEDRATE_SLOW (Z_PROBE_FEEDRATE_FAST / 2)
 
 /**
  * Probe Activation Switch
